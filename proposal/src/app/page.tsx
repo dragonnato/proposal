4ede5acc52280d3f2145e2c137ef9c02e8547dac
davidnato <dragon20000330@outlook.com>
2025-02-18 08:56:21 -0600

    First Commit

---
+++ proposal/src/app/page.tsx
@@ -0,0 +1,26 @@
+export default function Page() {
+  return (
+    <>
+      <div className="grid-cols-3">
+        <div className="flex flex-row">
+          <div className="max-h-52 bg-blue-950">
+            Coupon Deals
+          </div>
+          <div className="max-h-36 bg-black">
+            Stores |stores
+          </div>
+          <div className="max-h-36 bg-yellow-950">
+            Stores |stores
+          </div>
+        </div>
+      </div>
+      <div className="grid-cols-6">
+
+      </div>
+      <div className="grid-cols-3">
+
+      </div>
+    </>
+
+  )
+}
